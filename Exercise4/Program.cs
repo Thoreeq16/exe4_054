1fc500f2e22ccde5bdcb91462df7c7adb711ee9b
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2022-12-08 16:09:04 +0700

    add main method

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -59,5 +59,45 @@ namespace Exercise4
                 Console.WriteLine();
             }
         }
+        static void Main(string[] args)
+        {
+            Stacks s = new Stacks();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n**Stack Menu**\n");
+                Console.WriteLine("1. Push.");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your Choice: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a Number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '3':
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice0");
+                        break;
+                }
+            }
+        }
     }
 }
